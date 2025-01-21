14b01f449b9d5e22adcfefc3e8439be3b93241a2
lucasalmeida1702 <lucasalmeidasouza127@gmail.com>
2025-01-21 20:13:19 -0300

    Add files via upload

---
+++ INCLUDE/Cozinheiro (1).hpp
@@ -0,0 +1,5 @@
+#ifndef COZINHEIRO_HPP
+#define COZINHEIRO_HPP
+
+#include <string>
+#include <vector>
---
+++ INCLUDE/Dono (1).hpp
@@ -0,0 +1,6 @@
+#ifndef DONO_HPP
+#define DONO_HPP
+
+#include <string>
+#include <vector>
+#include "Cardapio.hpp"
