21c46141988fa32a6a60afc51800ba2826d2c768
PedroPina10 <pedropina2024@gmail.com>
2025-01-27 22:40:27 -0300

    Dono_restaurante_test.cpp

--- tests/Dono_restaurante_test.cpp
+++ tests/Dono_restaurante_test.cpp
@@ -1,1 +1,19 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "DonoRestaurante.hpp"
+#include "Pedido.hpp"
+#include <vector>
+#include <map>
+
+TEST_CASE("Testando DonoRestaurante") {
+    std::vector<Pedido*> pedidos;
+    Pedido pedido1("Cliente1", "Pedido feito");
+    Pedido pedido2("Cliente2", "Pedido feito");
+    pedidos.push_back(&pedido1);
+    pedidos.push_back(&pedido2);
+
+    DonoRestaurante dono(nullptr, &pedidos);
+    std::map<std::string, std::string> pedidosFeitos = {{"Cliente1", "Pizza"}, {"Cliente2", "Hamburguer"}};
+    dono.actions_dono(pedidosFeitos);
+}
 
